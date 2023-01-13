6368941d3a61075dbbdd4322748da3787c868e57
Mikhelmariya <mikhela65@gmail.com>
2023-01-14 00:49:32 +0530

    image added ,made first layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,30 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
-  runApp(ToDo());
-}
-
-class TodoList extends StatefulWidget {
-  @override
-  _TodoListState createState() =>
-      _TodoListState(); //TodoListState manages state
-}
-
-class _TodoListState extends State<TodoList> {
-  final List<String> _todoItems = [];
-  void _addtodoitems(String task) {
-    if (task.length > 0) {
-      setState(() {
-        _todoItems.add(task);
-      });
-    }
-  }
-
-  void _removetodoitems(int index) {
-    setState(() {
-      _todoItems.removeAt(index);
-    });
-  }
-
-  @override
-  Widget build(BuildContext) {
-    return ListView.builder(
-      itemCount: _todoItems.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(_todoItems[index]),
-          onLongPress: () => _removetodoitems(index),
-        );
-      },
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      home: TodoList(),
-    );
-  }
-}
-
-class ToDo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("To Do List")),
-          backgroundColor: Color(0xffff8f00),
-          foregroundColor: Colors.black87,
+  runApp(MaterialApp(
+    home: Column(
+      children: [
+        Container(
+          height: 250,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/todo.png"), fit: BoxFit.cover)),
         ),
-        backgroundColor: Color(0xffffecb3),
-        body: FloatingActionButton(
-          onPressed: () {},
+        Container(
+          height: 324,
+          color: Color(0xffF6F5F8),
+          child: FloatingActionButton(onPressed: (() {})),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-      ),
-    );
-  }
+        Expanded(
+            child: Column(children: [
+          Container(
+            color: Colors.amber,
+          )
+        ])),
+      ],
+    ),
+  ));
 }
