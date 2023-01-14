778143e7e9d717479bac1cd8606c30477189101e
Mikhelmariya <mikhela65@gmail.com>
2023-01-14 16:37:25 +0530

    stack positioned

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,44 @@ import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Column(
-      children: [
-        Container(
-          height: 250,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("images/todo.png"), fit: BoxFit.cover)),
-        ),
-        Container(
-          height: 324,
-          color: Color(0xffF6F5F8),
-          child: FloatingActionButton(onPressed: (() {})),
-        ),
-        Expanded(
-            child: Column(children: [
+    home: Scaffold(
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
           Container(
-            color: Colors.amber,
-          )
-        ])),
-      ],
+            color: Color(0xfff6f5f8),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 400,
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: Image.asset('images/todo.png'),
+            ),
+          ),
+          Positioned(
+            top: 500,
+            left: 100,
+            right: 100,
+            bottom: 10,
+            child: InkWell(
+              onTap: (() {}),
+              child: Container(
+                  height: 30,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0xff3d84d9),
+                    borderRadius: BorderRadius.circular(53.19),
+                  ),
+                  child: Center(
+                    child: Text("Add new Task"),
+                  )),
+            ),
+          ),
+        ],
+      ),
     ),
   ));
 }
