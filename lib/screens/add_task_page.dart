d3ddf7451db82693bf97db02201a02b77ee9311a
Mikhelmariya <mikhela65@gmail.com>
2023-01-17 11:51:14 +0530

    two column elements added

--- lib/screens/add_task_page.dart
+++ lib/screens/add_task_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,10 +10,42 @@ class AddTaskPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 594,
-      color: Color.fromARGB(255, 46, 10, 118),
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 863.81,
+          //width: 398.95,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(45.59),
+            color: Color(0xffF1F5F9),
+          ),
+        ),
+        Positioned(
+            child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 175.24, top: 31.58),
+              child: Text(
+                "Create new task",
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 21,
+                top: 59.42,
+              ),
+              child: Text(
+                "Task Title",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+              ),
+            )
+          ],
+        )),
+      ]),
     );
   }
 }
