fc0ad1f3f2e81066e360e0e4599f0bd64992cc9c
Mikhelmariya <mikhela65@gmail.com>
2023-01-25 16:27:16 +0530

    added new

--- lib/main.dart
+++ lib/main.dart
@@ -23,57 +23,102 @@ class Homepage extends StatelessWidget {
             color: Color(0xfff6f5f8),
             child: Column(
               children: [
-                Column(
-                  children: [
-                    Container(
-                      height: 250,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('images/todo.png'),
-                              fit: BoxFit.cover)),
-                      child: Column(children: [
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 17),
-                            child: Text("Task Manager",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.normal,
+                Column(children: [
+                  Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/todo.png'),
+                            fit: BoxFit.cover)),
+                    child: Column(children: [
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 17),
+                          child: Text("Task Manager",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white,
+                                  fontSize: 17)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 63.42),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.arrow_back_ios_rounded,
                                     color: Colors.white,
-                                    fontSize: 17)),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 63.42),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.arrow_back_ios_rounded,
-                                      color: Colors.white,
-                                    )),
-                                Text(
-                                  "December",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 28),
-                                ),
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.arrow_forward_ios_rounded,
-                                      color: Colors.white,
-                                    )),
-                              ]),
-                        ),
-                      ]),
+                                  )),
+                              Text(
+                                "December",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 28),
+                              ),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.arrow_forward_ios_rounded,
+                                    color: Colors.white,
+                                  )),
+                            ]),
+                      ),
+                    ]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 165),
+                    child: Text(
+                      "Completed",
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                  ],
+                  ),
+                ]),
+                SingleChildScrollView(
+                  child: Container(
+                    height: 70 * 2,
+                    width: 364,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(17)),
+                    child: ListView.separated(
+                        separatorBuilder: (context, index) {
+                          return Divider(
+                            height: 1,
+                            color: Colors.grey,
+                          );
+                        },
+                        itemCount: 2,
+                        itemBuilder: ((context, index) {
+                          return TaskList();
+                        })),
+                  ),
                 ),
+                // GestureDetector(
+                //   onTap: (() {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => const AddTaskPage()),
+                //     );
+                //   }),
+                //   child: Container(
+                //       alignment: Alignment.bottomCenter,
+                //       height: 59.58,
+                //       width: 364.96,
+                //       decoration: BoxDecoration(
+                //         color: Color(0xff3d84d9),
+                //         borderRadius: BorderRadius.circular(53.19),
+                //       ),
+                //       child: Center(
+                //         child: Text("Add new Task"),
+                //       )),
+                // )
               ],
             ),
           ),
